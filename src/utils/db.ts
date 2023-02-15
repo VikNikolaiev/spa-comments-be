7541597e66d53de9bf05090bc9721a80513b829f
Viktor Nikolaiev <viknikolaiev@gmail.com>
2023-02-15 12:09:43 +0200

    added dotenv

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -3,12 +3,18 @@ import * as dotenv from 'dotenv'
 
 dotenv.config();
 
+const { DB, USER, PASS, HOST } = process.env;
+
+if (!DB || !USER || !PASS || !HOST) {
+  throw new Error('Missing DB config');
+}
+
 export const sequelize = new Sequelize(
-  'grandins_dzencode',
-  'grandins_dzencode',
-  '5ks*vK7X+2',
+  DB,
+  USER,
+  PASS,
   {
-    host: 'grandins.mysql.tools',
+    host: HOST,
     dialect: 'mysql',
     dialectModule: require('mysql2'),
   },
